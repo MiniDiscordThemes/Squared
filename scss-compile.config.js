a9c4611a8d5ddf214be8e175ddf513d86ee692f3
Saltssaumure <saltssaumure@gmail.com>
2023-05-01 02:31:15 +0100

    Version bump for Replugged

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -8,6 +8,6 @@ export default {
         /** The name of your theme repository. */
         repo: "Squared",
         /** The version of your theme. */
-        version: "1.2.1"
+        version: "1.2.2"
     }
 };
