f120de8ef61e6b541905d5a2e574918e68700ca2
Saltssaumure <Saltssaumure@users.noreply.github.com>
2023-08-27 00:51:21 +0000

    deploy: 461dcd3690ef08b1c7b68ba6fb7d098d56cb5e02

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -1,13 +1,9 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
     meta: {
-        /** The official name of your theme. */
-        name: "Squared",
-        /** The name of your base scss file. */
-        scss: "sq",
-        /** The name of your theme repository. */
-        repo: "Squared",
-        /** The version of your theme. */
-        version: "1.2.8"
+        name: "Squared" /* The official name of your theme. */,
+        scss: "sq" /* The name of your base scss file. */,
+        repo: "Squared" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
     }
 };
